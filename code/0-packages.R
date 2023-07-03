5460ba4fa01662bc35a4e360e26f291bae71e650
Matthew Berens <matthewjberens@gmail.com>
2023-07-03 14:24:15 -0400

    big update

--- code/0-packages.R
+++ code/0-packages.R
@@ -13,9 +13,22 @@ library(knitr)
 library(reshape2)
 library(ggExtra)
 library(stringi)
-library(nlme)
+library(sf)
+library(raster)
+library(nhdplusTools)
+library(FedData)
 library(magrittr)
+library(mapview)
+mapviewOptions(
+  basemaps = c("CartoDB.Positron", "OpenTopoMap", "Esri.WorldImagery", "Esri.WorldShadedRelief"),
+  homebutton = FALSE,
+  query.position = "topright",
+  query.digits = 2,
+  query.prefix = "",
+  legend.pos = "bottomright"
+)
 
+library(exactextractr)
 
 
 # functions 
@@ -27,54 +40,8 @@ theme_mb1 <- function() {  # this for all the elements common across plots
           legend.text = element_text(size = 12),
           legend.key.size = unit(1.5, 'lines'),
           panel.border = element_rect(color="black",size=2, fill = NA),
+          panel.grid = element_blank(),
           
-          plot.title = element_text(hjust = 0.5, size = 14),
-          plot.subtitle = element_text(hjust = 0.5, size = 12, lineheight = 1.5),
-          axis.text = element_text(size = 12, color = "black"),
-          axis.title = element_text(size = 12, face = "bold", color = "black"),
-          
-          # formatting for facets
-          panel.background = element_blank(),
-          strip.background = element_rect(colour="white", fill="white"), #facet formatting
-          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
-          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
-          strip.text.x = element_text(size=12, face="bold"), #facet labels
-          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
-    )
-}
-
-theme_mb2 <- function() {  # this for all the elements common across plots
-  theme_bw() %+replace%
-    theme(legend.position = "top",
-          # legend.key=element_blank(),
-          # legend.title = element_blank(),
-          legend.text = element_text(size = 12),
-          legend.key.size = unit(1.5, 'lines'),
-          panel.border = element_rect(color="black",size=2, fill = NA),
-          
-          plot.title = element_text(hjust = 0.5, size = 14),
-          plot.subtitle = element_text(hjust = 0.5, size = 12, lineheight = 1.5),
-          axis.text = element_text(size = 14, color = "black"),
-          axis.title = element_text(size = 14, face = "bold", color = "black"),
-          
-          # formatting for facets
-          panel.background = element_blank(),
-          strip.background = element_rect(colour="white", fill="white"), #facet formatting
-          panel.spacing.x = unit(1.5, "lines"), #facet spacing for x axis
-          panel.spacing.y = unit(1.5, "lines"), #facet spacing for x axis
-          strip.text.x = element_text(size=12, face="bold"), #facet labels
-          strip.text.y = element_text(size=12, face="bold", angle = 270) #facet labels
-    )
-}
-
-theme_mbclean <- function() {  # this for all the elements common across plots
-  theme_clean() %+replace%
-    theme(legend.position = "right",
-          legend.key=element_blank(),
-          legend.title = element_blank(),
-          legend.text = element_text(size = 12),
-          legend.key.size = unit(1.5, 'lines'),
-          panel.border = element_rect(color="white",size=0, fill = NA),
           plot.title = element_text(hjust = 0.5, size = 14),
           plot.subtitle = element_text(hjust = 0.5, size = 12, lineheight = 1.5),
           axis.text = element_text(size = 12, color = "black"),
