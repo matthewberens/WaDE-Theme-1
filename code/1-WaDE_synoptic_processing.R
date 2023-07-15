71c6020031a02f44f3e9235b8f3c3aad8c092336
Matthew Berens <matthewjberens@gmail.com>
2023-07-15 17:28:58 -0400

    add ICP-MS data

---
+++ code/1-WaDE_synoptic_processing.R
@@ -0,0 +1,69 @@
+#############################################
+# WaDE-SFA Synoptic Sampling Data Processing
+# Matthew Berens
+# Updated June 2023
+#############################################
+
+#Load libraries and set directory
+setwd("~/Documents/GitHub/Wade-Theme-1")
+
+#load packages
+source("code/0-packages.R")
+
+# Step 1. Load Data ---------------------------------------------------------------------
+
+sampling_data = read.csv("raw/WaDE SYNOPTIC_2023-06-23.csv") 
+
+
+
+# Step 2. Format Date and Time  ---------------------------------------------------------
+
+sampling_data <- sampling_data %>%
+  mutate(sample_date = lubridate::mdy(sample_date),
+         sample_time = lubridate::hm(sample_time),
+         Year = lubridate::year(sample_date),
+         DOY = lubridate::yday(sample_date))
+
+
+
+# Step 3. Calculate TIC  ----------------------------------------------------------------
+
+sampling_data$DIC = sampling_data$TC - sampling_data$DOC
+
+
+
+
+# Step 4. Transform data and reshape data -----------------------------------------------
+
+data_transform <- sampling_data %>%
+  pivot_longer(-c("site_name":"medium", "Year","DOY"), values_to = "result_value", names_to = "parameter") %>%
+  mutate(unit = ifelse(parameter == "depth", "CM",
+                ifelse(parameter == "temp", "C",
+                ifelse(parameter == "pH", "UNITS",
+                ifelse(parameter == "SpC", "uS/CM", "MG/L")))),
+         constituent = ifelse(parameter == "depth", "PHYS",
+                       ifelse(parameter %in% c("pH", "SpC", "temp"), "CHEM",
+                       ifelse(parameter %in% c("DOC", "TC", "DIC"), "C",
+                       ifelse(parameter %in% c("SO4", "Cl", "NO3", "PO4"), "ANION", "METALS")))))
+
+
+
+# Step 5. Determine detection limit flags -----------------------------------------------
+
+#Read in instrument detection limits
+WaDE_ddl = read.csv("raw/WaDE DETECTION LIMITS.csv") 
+
+#Merge ddls with analytical results and determine if flagged
+data_LOD <- left_join(data_transform, WaDE_ddl, by = "parameter") %>%
+  mutate(detection_FLAG = ifelse(parameter %in% c("pH", "temp", "SpC", "depth"), "NONE",
+                          ifelse(result_value < ddl, "<", "NONE")),
+         result_value = ifelse(detection_FLAG == "<", ddl/2, result_value),
+         result_value = ifelse(constituent == "METALS", result_value * 1000, result_value)) #Adjust units for metals to MG/L
+
+# Step 6. Export processed data as csv --------------------------------------------------
+
+#Save formatted data as a .csv
+write.csv(data_LOD, "processed/SYNOPTIC_2023-06-23.csv", row.names = FALSE)
+  
+
+
--- code/WaDE_map_forPrint.R
+++ code/WaDE_map_forPrint.R
@@ -22,7 +22,7 @@ WaDE_sites <- read.csv("raw/WaDE SYNOPTIC_locations.csv") %>%
   st_set_crs(4326)
 
 # Step 2. Load Synoptic Data, if desired ----------------------------------------------------
-#synoptic_data <- read.csv("processed/SYNOPTIC_2023-06-23.csv")
+synoptic_data <- read.csv("processed/SYNOPTIC_2023-06-23.csv")
 
 
 
@@ -34,26 +34,28 @@ merged_siteData <- merge(WaDE_sites, synoptic_data, by = "site_name")
 
 # Step 3. Create map of locations -----------------------------------------------------------
 #If you want to save the map, uncomment Line 29 and give the map a name.
-#map_name <- 
+map_name <- 
   
 ggplot() +
 
 #Begin with the EFPC watershed outline 
   geom_sf(data = EFPC_HUC, fill = NA, col = "#0E4C92", linewidth = 2) +
+  new_scale_fill() +
   
 #-------------------
 #For lines 42-59, select the 2-line pair that corresponds to the land use that you want to show.  
  
 #Use this line for no fill
-  geom_sf(data = EFPC_catchments_landuse, fill = NA, col = "black", linetype = "dotted") +
+  #geom_sf(data = EFPC_catchments_landuse, fill = NA, col = "black", linetype = "dotted") +
   
 #Use these two lines for forested land cover
   #geom_sf(data = EFPC_catchments_landuse, aes(fill = Forest), col = "black", linetype = "dotted") +
   #scale_fill_gradientn(colours = terrain.colors(50), trans = 'reverse') +
 
 #Use these two lines for developed land cover 
-  #geom_sf(data = EFPC_catchments_landuse, aes(fill = Developed), col = "black", linetype = "dotted") +
+  geom_sf(data = EFPC_catchments_landuse, aes(fill = Developed), col = "black", linetype = "dotted") +
   #scale_fill_distiller(palette  = "Reds", direction = 1) +
+  scale_fill_gradient(low = "#4A9E48", high = "red") +
   
 #Use these two lines for wetland land cover 
   #geom_sf(data = EFPC_catchments_landuse, aes(fill = Wetland), col = "black", linetype = "dotted") +
@@ -73,13 +75,16 @@ ggplot() +
   new_scale_fill() +
   
 #Run line 71 to just plot sampling locations
-  #geom_sf(data = WaDE_sites, aes(fill = network) , shape = 21, size = 4) +
-
-#Run line 74-75 to show sampling results. Change "parameter" to the paramater of interest. 
-  geom_sf(data = merged_siteData %>% subset(parameter == "pH"), aes(fill = result_value) , shape = 21, size = 4) +
-  scale_fill_distiller(palette  = "RdYlBu", direction = -1) +
+  geom_sf(data = WaDE_sites, aes(fill = network), shape = 21, size = 4) +
+  scale_fill_brewer(palette  = "Set1") +
   
-  labs(fill = "pH") + #Name the legend for sampling location fill colors
+#Run line 74-75 to show sampling results. Change "parameter" to the paramater of interest. 
+  #geom_sf(data = merged_siteData %>% subset(parameter == "Ca" & !is.na(result_value)), aes(fill = result_value) , shape = 21, size = 4) +
+  #geom_sf(data = merged_siteData %>% subset(parameter == "depth"), aes(fill = flow_status) , shape = 21, size = 6) +
+  #scale_fill_distiller(palette  = "RdYlBu", direction = -1) +
+  #scale_fill_manual(values = c("#FF0505", "#9AC75D", "#FFCD05")) +
+  
+  #labs(fill = NULL) + #Name the legend for sampling location fill colors
   
 #Add map annotations, scales, and legends 
   theme_classic() +
@@ -94,5 +99,5 @@ ggplot() +
       line_col = "black"))
 
 # Step 4. Save the map -----------------------------------------------------------
-#If you want to output the map, uncomment Line 67 and replace "map_name" with the name from Line 29.
-#ggsave("output/map_name.png", map = map_name, width = 12, height = 9, units = "in")
+#If you want to output the map, un-comment Line 67 and replace "map_name" with the name from Line 29.
+ggsave("map_name.eps")
